785db080b965f68342867a8b7708fe95bae4c29c
Ibrahim Tasyurt <ibrahim.tasyurt@udemy.com>
2020-12-28 17:56:55 +0300

    package name change and format

--- src/test/kotlin/AnalyzerTest.kt
+++ src/test/kotlin/AnalyzerTest.kt
@@ -1,16 +1,15 @@
 package com.github.itasyurt.elastic.analyzers
+
 import org.junit.Assert
 import org.junit.Test
 
-class AnalyzerTest
-{
+class AnalyzerTest {
     @Test
     fun testTokensAnalyzed() {
 
         val analyzer = ElasticAnalyzer()
 
         val tokens = analyzer.analyze("A Blog Post on <b>Elasticsearch</b>")
-
         Assert.assertEquals(listOf("blogpost", "elasticsearch"), tokens)
 
     }
